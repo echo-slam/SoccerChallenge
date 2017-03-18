91378b4e85dddb8221f8930ccab1fa0a0b2d9b1b
Minh Nguyen <ng.huyminh91@gmail.com>
2017-03-18 22:32:36 +0700

    Update schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170318140514) do
+ActiveRecord::Schema.define(version: 20170318141947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,26 @@ ActiveRecord::Schema.define(version: 20170318140514) do
     t.string   "venue"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+  end
+
+  create_table "match_requests", force: :cascade do |t|
+    t.integer  "match_id"
+    t.integer  "team_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "matches", force: :cascade do |t|
+    t.integer  "team_owner_id"
+    t.integer  "team_away_id"
+    t.integer  "field_id"
+    t.datetime "starts_at"
+    t.datetime "ends_at"
+    t.boolean  "is_start"
+    t.integer  "home_goal"
+    t.integer  "away_goal"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "players", force: :cascade do |t|
