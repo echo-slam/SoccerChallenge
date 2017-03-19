f16d74a7be2d507089ea1d04ad09234634a78a6e
Minh Nguyen <ng.huyminh91@gmail.com>
2017-03-19 12:11:43 +0700

    Add create method for player and team
    Signed-off-by: Minh Nguyen <ng.huyminh91@gmail.com>

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,2 +1,23 @@
 class TeamsController < ApplicationController
+  def new
+    @team_owner = TeamOwner.find_by(:player_id => current_player.id)
+    @team = @team_owner.build_team
+  end
+
+  def create
+    @team_owner = TeamOwner.find_by(:player_id => current_player.id)
+    @team = @team_owner.build_team(team_params)
+    if @team.save?
+      flash[:success] = "Successfully create team"
+      redirect_to root_path
+    else
+      flash[:error] = @team.errors.full_messages.to_sentence
+      render 'new'
+    end
+  end
+
+  private
+    def team_params
+      params.require(:team).permit(:name)
+    end
 end
