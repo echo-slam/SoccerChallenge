a275ec044a58e66981ab5ad80b494b9472f27fdf
Lanh Hoang <lanh.hoangcong@gmail.com>
2017-03-20 16:49:23 +0700

    Add Sign In, Create Team views

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,22 +5,22 @@ class SessionsController < ApplicationController
   def create
     if params[:sign_in_field_owner]
 
-      if @field_owner = FieldOwner.find_by(:email => params[:email]) and @field_owner.authenticate(params[:password])
+      if @field_owner = FieldOwner.find_by(email: params[:email]) and @field_owner.authenticate(params[:password])
         flash[:success] = "Field owner sign in successfully"
         session[:field_owner_id] = @field_owner.id
         redirect_to root_path
       else
-        flash[:error] = "Invalid username or password"
+        flash[:error] = "Field Owner: Invalid username or password"
         render 'new'
       end
 
     elsif params[:sign_in_player]
-      if @player = Player.find_by(:email => params[:email]) and @player.authenticate(params[:password])
+      if @player = Player.find_by(email: params[:email]) and @player.authenticate(params[:password])
         flash[:success] = "Player sign in successfully"
         session[:player_id] = @player.id
         redirect_to root_path
       else
-        flash[:error] = "Invalid username or password"
+        flash[:error] = "Player: Invalid username or password"
         render 'new'
       end      
     end
--- app/models/team.rb
+++ app/models/team.rb
@@ -2,5 +2,7 @@ class Team < ApplicationRecord
   has_many :players
   belongs_to :team_owner
 
+  validates :name, presence: true, uniqueness: true
+
   #accepts_nested_attributes_for :players
 end
