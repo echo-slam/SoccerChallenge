0276304eec40243b8b3d35bcbdc511136372e21f
Minh Nguyen <ng.huyminh91@gmail.com>
2017-03-21 16:14:03 +0000

    Update database for MatchRequest model
    * Change migration column to requested_team_id
    
    Signed-off-by: Minh Nguyen <ng.huyminh91@gmail.com>

--- db/migrate/20170321070250_add_status_team_request_to_match_requests.rb
+++ db/migrate/20170321070250_add_status_team_request_to_match_requests.rb
@@ -1,6 +1,6 @@
 class AddStatusTeamRequestToMatchRequests < ActiveRecord::Migration[5.0]
   def change
-    add_column :match_requests, :team_request_id, :integer
+    add_column :match_requests, :requested_team_id, :integer
     add_column :match_requests, :status, :string, :default => 'PENDING'
   end
 end
