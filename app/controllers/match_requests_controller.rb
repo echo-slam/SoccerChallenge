5826ef618825e447e5fd3bae3567003ee888fad6
Minh Nguyen <ng.huyminh91@gmail.com>
2017-03-22 07:53:11 +0000

    Add invite feature for match_request

--- app/controllers/match_requests_controller.rb
+++ app/controllers/match_requests_controller.rb
@@ -1,12 +1,14 @@
 class MatchRequestsController < ApplicationController
 
   def create
+    # Join the match
     if params[:match_id]
-      @team = Team.find_by_id(current_player.team_owner.team_id)
-      @match = Match.find_by_id(params[:match_id])
+      # Assume that the "join" button pass the params match_id when clicked
+      @team = Team.find(current_player.team_owner.team_id)
+      @match = Match.find(params[:match_id])
       @match_request = @team.match_requests.build
       
-      @match_request.match_id = params[:match_id]
+      @match_request.match_id = @match.id
       @match_request.requested_team_id = @match.team_owner_id
       
       if @match_request.save
@@ -15,6 +17,25 @@ class MatchRequestsController < ApplicationController
         flash[:error] = @match_request.errors_full_messages.to_sentence
       end
       redirect_to matches_path
+      
+    # Invite team to the match
+    elsif params[:invite_id]
+      # Assume that the "invite" button is in MATCH_PATH(:id)
+      # The invite_id will be pass to this controller when clicked the invite btn
+      @team = Team.find(current_player.team_owner.team_id)
+      @match = Match.find(params[:id])
+      @match_request = @team.match_requests.build
+      
+      @match_request.match_id = @match.id
+      @match_request.requested_team_id = params[:invite_id]
+      
+      if @match_request.save
+        flash[:success] = "The invitation is sent"
+      else
+        flash[:error] = @match_request.errors.full_messages.to_sentence
+      end
+      redirect_to match_path(@match)
     end
+    
   end
 end
