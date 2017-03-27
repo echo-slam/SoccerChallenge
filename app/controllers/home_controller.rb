e2bdd7b9f9f16556d87067ad27785239c2921e15
huyphung1602 <huy.phung.sw@gmail.com>
2017-03-27 18:08:08 +0700

    Upgrade UI, add signed_in_index, new background

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,15 @@
 class HomeController < ApplicationController
   def index
     if current_player
-      redirect_to teams_path
+      redirect_to signed_in_index_path
     else
       respond_to do |format|
         format.html
         format.js
       end
     end
+  end
+
+  def signed_in_index
   end
 end
