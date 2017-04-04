ae1e7469d85ee5e67e4ae31e9b78b3193c29567d
Minh Nguyen <ng.huyminh91@gmail.com>
2017-04-05 00:24:00 +0700

    Update fetch function to fetch more data

--- app/services/fetch_page.rb
+++ app/services/fetch_page.rb
@@ -2,23 +2,30 @@ class FetchPage
 
   def fetch_webpage
     url = "http://tdp.bongda24h.vn/nhan-vat-c389-p1.html"
+
     doc = Nokogiri::HTML(open(url))
 
     url_list = []
-    doc.css(".box-ban-doc .list_tintuc-item:nth-child(1) .td_tintuc_h3 , .post-title-entry a").each do |link|
+    fetch_data = []
+
+    doc.css(".loadmore .td_tintuc_h3 a").each do |link|
       url_list.push(link["href"])
+      #puts link["href"]
     end
 
-    url_list.reject!(&:nil?)
-    article_url = url_list.join('')
+    url_list.each do |link|
+      link = URI.encode(link)
 
-    doc = Nokogiri::HTML(open(article_url))
-    #content = doc.css(".post-title-singer , #singer-entry div+ div , strong").to_html
-    content = doc.css("p+ div strong").text
+      doc = Nokogiri::HTML(open(link))
+      article_title = doc.css(".post-title-singer").text
+      article_content = doc.css("p+ div strong").text
+
+      data = {title: article_title, content: article_content, url: link}
+      fetch_data.push(data)
+    end
+
+    fetch_data
     
-    title = doc.css(".post-title-singer").text
-    #fetch_data = {}
-    fetch_data = {title: title, content: content, url: article_url}
   end 
 
 end
--- app/views/articles/index.json.jbuilder
+++ app/views/articles/index.json.jbuilder
@@ -1,7 +1,3 @@
 json.set! :seed_article do
-  json.title @data_fetch[:title]
-  json.content @data_fetch[:content]
-  json.url @data_fetch[:url]
-
+  json.array! @data_fetch
 end
-
