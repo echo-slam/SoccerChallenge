d3c66fea0d3e528035760fe32413f94dee58a279
Minh Nguyen <ng.huyminh91@gmail.com>
2017-04-04 22:59:53 +0700

    Add seed json data

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,13 @@
+require "#{Rails.root}/app/services/fetch_page.rb"
+
 class Article < ApplicationRecord
   belongs_to :player
   validates :title, presence: true
   validates :image_url, presence: true
   validates :body, presence: true
+
+  def self.seed_article
+    fetch_obj = FetchPage.new()
+    data_fetch = fetch_obj.fetch_webpage
+  end
 end
---
+++ app/views/articles/index.json.jbuilder
@@ -0,0 +1,7 @@
+json.set! :seed_article do
+  json.title @data_fetch[:title]
+  json.content @data_fetch[:content]
+  json.url @data_fetch[:url]
+
+end
+
