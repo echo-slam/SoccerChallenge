ceb0e81d88ab2a9e4755c3eac58804f4176b2471
Lanh Hoang <lanh.hoangcong@gmail.com>
2017-04-05 22:58:53 +0700

    OAuthFacebook: define Facebook API version

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,9 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'],
+    client_options: {
+      site: 'https://graph.facebook.com/v2.8',  # this is the example API version
+      authorize_url: "https://www.facebook.com/v2.8/dialog/oauth"
+    }
 end
